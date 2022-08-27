b9e578ebd275403c0fb01a9b7b568b08ae0ef8d8
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-27 13:17:23 +0800

    Delete test directory

--- test/puzzles/podofdolphins.jl
+++
@@ -1,6 +0,0 @@
-@testset "pod of dolphins" begin
-    # http://web.mit.edu/puzzle/www/2015/puzzle/pod_of_dolphins_meta/
-    clues = ["citygates", "impulsive", "clickspam", "baptistry", "leviathan", "policecar", "coupdetat", "sforzando", "cartwheel"]
-    results = evaluate(model, clues)
-    @test description(first(filter(all_identical, results))) == "Number of repeated letters"
-end
--- test/puzzles/rubik.jl
+++
@@ -1,5 +0,0 @@
-# http://web.mit.edu/puzzle/www/2013/coinheist.com/rubik/clockwork_orange/index.html
-@testset "rubik clockwork orange meta" begin
-    results = evaluate(model, ["armoredrecon", "hypapante", "commemorativebats", "derricktruck", "brownrot", "attorneysgeneral", "sacrosanct", "impromptu"])
-    @test "Number of repeated letters" in description.(results[1:3])
-end
