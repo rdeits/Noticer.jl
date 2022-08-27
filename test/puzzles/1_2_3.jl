ceb6c4efaea30a494bbcb1ea259d276fcd3dc5ee
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-27 13:22:26 +0800

    Delete test directory

--- test/puzzles/1_2_3.jl
+++
@@ -1,6 +0,0 @@
-# https://puzzles.mit.edu/2016/puzzle/1_2_3/solution/
-@testset "1, 2, 3" begin
-    clues = ["season", "saveup", "ecowas", "ignore", "sluice", "hosni", "inbed", "barbeau", "museum", "tobiah", "unsew", "dolce", "anaphia", "teenage"]
-    results = evaluate(model, clues)
-    @test description(first(results)) == "Number of consonants"
-end
--- test/puzzles/lastresort.jl
+++
@@ -1,5 +0,0 @@
-@testset "last resort" begin
-    # http://www.maths.usyd.edu.au/ub/sums/puzzlehunt/2016/puzzles/A2S1_Last_Resort.pdf
-    results = evaluate(model, ["advent", "achilles", "binary", "norway", "bubbly", "yacht", "anchor"])
-    @test description(first(results)) == "Number of reverse alphabetical bigrams"
-end
